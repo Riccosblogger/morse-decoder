b62d33d51f30292a491464426cbbfed89cc079dd
Riccosblogger <riccosblogger@gmail.com>
2020-03-07 12:45:45 +0300

    feat: add the solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,34 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    
+    var arr=[], temp, result_str='', result = [];
+    
+    for (let i=0; i<expr.length; i+=10)
+    arr.push(expr.substr(i, 10));
+    
+    
+    for (let i=0; i<arr.length; i++){
+        temp = [];
+        for (let j=0; j<arr[0].length; j+=2) {
+           if (arr[i][j] == '1' && arr[i][j+1] == '0')
+          temp.push('.');
+        if (arr[i][j] == '1' && arr[i][j+1] == '1')
+          temp.push('-');
+    }
+    
+    result.push(temp.join(''));
+    
+    }
+    
+    for (i in result){
+        if (!result[i]) result_str += ' ';
+        else result_str += MORSE_TABLE[result[i]];
+    }
+    
+    return result_str;
+    }
 
-module.exports = {
-    decode
-}
+    module.exports = {
+        decode
+    }
